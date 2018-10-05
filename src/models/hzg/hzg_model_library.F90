a75cb89a255c5f9996a6c975c822deebd22f5ee0
Richard Hofmeister <richard.hofmeister@hzg.de>
2018-10-05 17:13:03 +0200

    added hzg_test model, put hzg models into model factory

---
+++ src/models/hzg/hzg_model_library.F90
@@ -0,0 +1,38 @@
+module hzg_model_library
+
+   use fabm_types, only: type_base_model_factory,type_base_model
+
+   implicit none
+
+   private
+
+   type,extends(type_base_model_factory) :: type_factory
+      contains
+      procedure :: create
+   end type
+
+   type (type_factory),save,target,public :: hzg_model_factory
+
+contains
+
+   subroutine create(self,name,model)
+
+      use fabm_hzg_ecosmo
+      use fabm_hzg_omexdia_p
+      use fabm_hzg_test
+      ! Add your own model module here...
+
+      class (type_factory),intent(in) :: self
+      character(*),        intent(in) :: name
+      class (type_base_model),pointer :: model
+
+      select case (name)
+         case ('ecosmo');       allocate(type_hzg_ecosmo::model)
+         case ('omexdia_p');    allocate(type_hzg_omexdia_p::model)
+         case ('test');         allocate(type_hzg_test::model)
+         ! Add your new model here
+      end select
+
+   end subroutine
+
+end module
