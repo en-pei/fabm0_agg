a77b44a4cc6c4f849cdae54a4a150595da4a6c23
Déborah  Benkort <deborahbenkort@Deborahs-MacBook-Pro.local>
2019-05-15 07:37:47 +0200

    cleaning code and addition of new nutrient dynamic

--- src/models/hzg/icelight/icelight.F90
+++ src/models/hzg/icelight/icelight.F90
@@ -20,12 +20,17 @@ module hzg_icelight
       ! Identifiers for diagnostic variables [model outputs]
       type (type_diagnostic_variable_id)            :: id_par  ! Photosynthetically active radiation
       type (type_diagnostic_variable_id)            :: id_swr  ! Shortwave radiation
-      type (type_horizontal_diagnostic_variable_id) :: id_par0 ! Surface photosynthetically active radiation
+      type (type_horizontal_diagnostic_variable_id):: id_par0 ! Surface photosynthetically active radiation
 
       ! Parameters
       real(rk) :: a,g1,g2
       real(rk) :: gice
       logical  :: use_snow
+      real(rk) :: C0
+      real(rk) :: Catt_ice
+      real(rk) :: Catt_sno
+      real(rk) :: ice_alb
+      real(rk) :: sno_alb
    contains
 !     Model procedures
       procedure :: initialize
@@ -50,7 +55,11 @@ contains
       call self%get_parameter(self%g2,'g2','m','e-folding depth of visible fraction',        default=23.0_rk) 
       call self%get_parameter(self%gice,'g_ice','m','e-folding depth of radiation in ice',        default=0.1_rk) 
       call self%get_parameter(self%use_snow,'use_snow','true/false','switch use of snow thickness', default=.false.) 
-
+      call self%get_parameter(self%C0,      'C0',      '',                       'part of incoming radiation absorbed',            default=0.3_rk)
+      call self%get_parameter(self%Catt_ice,'Catt_ice','1/m',                    'ice attenuation coefficent',                     default=0.5_rk)
+      call self%get_parameter(self%Catt_sno,'Catt_sno','1/m',                    'snow attenuation coefficent',                    default=0.5_rk)
+      call self%get_parameter(self%ice_alb, 'ice_alb', '',                       'albedo in the ice',                              default=0.5_rk)
+      call self%get_parameter(self%sno_alb, 'sno_alb', '',                       'albedo in the snow',                             default=0.5_rk)
       ! Register diagnostic variables
       call self%register_diagnostic_variable(self%id_swr,'swr','W/m^2','shortwave radiation', &
               standard_variable=standard_variables%downwelling_shortwave_flux,source=source_do_column)
@@ -58,8 +67,11 @@ contains
               standard_variable=standard_variables%downwelling_photosynthetic_radiative_flux,source=source_do_column)
       call self%register_diagnostic_variable(self%id_par0,'par0','W/m^2','surface photosynthetically active radiation', &
               standard_variable=standard_variables%surface_downwelling_photosynthetic_radiative_flux,source=source_do_column)
+      !call self%register_diagnostic_variable(self%id_par0ice,'par0ice','W/m^2','surface photosynthetically active radiation', &
+      !        standard_variable=standard_variables%surface_downwelling_photosynthetic_radiative_flux,source=source_do_column)
 
       ! Register environmental dependencies (temperature, shortwave radiation)
+      !call self%register_dependency(self%id_iceh,       'iceh', 'm',    'ice thickness')
       call self%register_dependency(self%id_swr0,standard_variables%surface_downwelling_shortwave_flux)
       call self%register_dependency(self%id_ext, standard_variables%attenuation_coefficient_of_photosynthetic_radiative_flux)
       call self%register_dependency(self%id_dz,  standard_variables%cell_thickness)
@@ -75,17 +87,26 @@ contains
 
       real(rk) :: swr0,dz,swr,par,z,ext,bioext
       real(rk) :: iceh, icec, snowh, par0ice, swr0ice
-
+      real(rk) :: alb
+ 
       _GET_HORIZONTAL_(self%id_swr0,swr0)
       _GET_HORIZONTAL_(self%id_iceh,iceh)
       _GET_HORIZONTAL_(self%id_icec,icec)
       if (self%use_snow) then
         _GET_HORIZONTAL_(self%id_snowh,snowh)
       end if
-
+      
+      icec = 1.0_rk
       ! so far, snow thickness not used
       swr0ice = swr0*(1-icec) + icec*swr0*exp(-iceh/self%gice)
       par0ice = swr0ice*(1-self%a)
+!      if(snowh > 0.0_rk) then 
+! 	alb = self%sno_alb
+!      else 
+! 	alb = self%ice_alb
+!      end if 
+
+!      par0ice = (1-icec) * (1.0_rk - alb) * self%C0 * exp((-self%Catt_ice * iceh) - (self%Catt_sno * snowh))
 
       _SET_HORIZONTAL_DIAGNOSTIC_(self%id_par0,par0ice)
       z = 0
@@ -108,6 +129,7 @@ contains
 
          _SET_DIAGNOSTIC_(self%id_swr,swr) ! Shortwave radiation at layer centre
          _SET_DIAGNOSTIC_(self%id_par,par) ! Photosynthetically active radiation at layer centre
+       !  _SET_DIAGNOSTIC_(self%id_par0ice,par0ice) ! Photosynthetically active radiation at bottom ice
       _VERTICAL_LOOP_END_
 
    end subroutine get_light
