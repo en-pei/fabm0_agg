e23084b3503d557b2bd6cd8659fb54e2cc98341b
Carsten Lemmen <carsten.lemmen@hzg.de>
2019-07-18 13:45:50 +0200

    Changed FABM_HOST mossco driver to resemble that of GETM

--- src/drivers/mossco/fabm_driver.h
+++ src/drivers/mossco/fabm_driver.h
@@ -1,25 +1,9 @@
 #define _FABM_DIMENSION_COUNT_ 3
 #define _FABM_DEPTH_DIMENSION_INDEX_ 3
+#define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
 
-! Include FABM preprocessor definitions.
-! This *must* be done after the host-specific variables are defined (above),
-! because these are used in fabm.h.
+#define _FABM_MASK_TYPE_ integer
+#define _FABM_UNMASKED_VALUE_ 1
+
 #include "fabm.h"
 
-! Taken from the General Ocean Turbulence Model:
-
-#define PATH_MAX	255
-
-#define stderr		0
-#define stdout		6
-
-#define STDOUT write(stdout,*)
-#define STDERR write(stderr,*)
-#define LEVEL0 STDERR
-#define LEVEL1 STDERR '   ',
-#define LEVEL2 STDERR '       ',
-#define LEVEL3 STDERR '           ',
-#define LEVEL4 STDERR '               ',
-#define FATAL  STDERR 'FATAL ERROR: ',
-
-#define LINE "------------------------------------------------------------------------"
