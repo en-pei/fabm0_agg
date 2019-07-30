9526acc8c2e79a9931e77e9b08291f39f7a42b33
Carsten Lemmen <carsten.lemmen@hzg.de>
2019-07-30 18:44:52 +0200

    Merged mossco-kse branch and KSE models

--- src/models/hzg/hzg_model_library.F90
+++ src/models/hzg/hzg_model_library.F90
@@ -17,13 +17,33 @@ contains
 
    subroutine create(self,name,model)
 
-      use fabm_hzg_ecosmo
+      ! Common models
       use hzg_omexdia_p
+      use fabm_hzg_ecosmo
+
+      ! KST specific models
       use fabm_hzg_test
       use fabm_hzg_icealgea
       use hzg_icelight
       use hzg_uvlight
-      ! Add your own model module here...
+
+      ! KSE specific models
+      !use hzg_omexdia_p_mpb
+      !use hzg_omexdia_cnp
+      !use hzg_omexdia_mpb
+      use hzg_mpb
+      !use hzg_mpb_cnp
+      use hzg_jelly
+      use hzg_n2pzdq
+      use hzg_medmac
+      use hzg_maecs
+      use hzg_benthic_pool
+      use hzg_Ndepoden
+      use fabm_hzg_dependencies
+      !use hzg_agg
+      !use hzg_kristineb
+
+      ! Add more models modules here
 
       class (type_factory),intent(in) :: self
       character(*),        intent(in) :: name
@@ -36,7 +56,22 @@ contains
          case ('icealgea');     allocate(type_hzg_icealgea::model)
          case ('icelight');     allocate(type_hzg_icelight::model)
          case ('uvlight');      allocate(type_hzg_uvlight::model)
-         ! Add your new model here
+         !case ('omexdia_p_mpb'); allocate(type_hzg_omexdia_p_mpb::model)
+         !case ('omexdia_cnp'); allocate(type_hzg_omexdia_cnp::model)
+         !case ('omexdia_mpb'); allocate(type_hzg_omexdia_mpb::model)
+         case ('mpb'); allocate(type_hzg_mpb::model)
+         !case ('mpb_cnp'); allocate(type_hzg_mpb_cnp::model)
+         case ('jelly'); allocate(type_hzg_jelly::model)
+         case ('n2pzdq'); allocate(type_hzg_n2pzdq::model)
+         case ('maecs'); allocate(type_hzg_maecs::model)
+         case ('medmac'); allocate(type_hzg_medmac::model)
+         !case ('kristineb'); allocate(type_hzg_kristineb::model)
+         case ('Ndepoden'); allocate(type_hzg_Ndepoden::model)
+         case ('benthic_pool'); allocate(type_hzg_benthic_pool::model)
+         case ('dependencies'); allocate(type_hzg_dependencies::model)
+         !case ('agg'); allocate(type_hzg_agg::model)
+         ! Add your model type here, and make sure
+         ! they appear in the use statement above
       end select
 
    end subroutine
