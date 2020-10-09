324309dbc94a36908f71441bc5c51a07fabdd88f
Johannes Timm <johannes.timm@hzg.de>
2020-10-09 18:34:06 +0200

    added most of lina equations

--- src/models/hzg/lina/lina.f90
+++ src/models/hzg/lina/lina.f90
@@ -307,24 +307,24 @@ subroutine do(self,_ARGUMENTS_DO_)
 !-----------------------------------
 !Enter potential spatial loops
 
-  !_FABM_LOOP_BEGIN_
-  _ARGUMENTS_DO_
-  _LOOP_BEGIN_
+   !_FABM_LOOP_BEGIN_
+   _ARGUMENTS_DO_
+   _LOOP_BEGIN_
 
 !----------------------------------
 !get inital values for external and state variables
 
 
-_GET_(self%id_lina_X, var%lina_X)
-_GET_(self%id_lina_A, var%lina_A)
-_GET_(self%id_lina_E, var%lina_E)
-_GET_(self%id_lina_L, var%lina_L)
-_GET_(self%id_lina_D, var%lina_D)
-_GET_(self%id_lina_N, var%lina_N)
-_GET_(self%id_lina_P, var%lina_P)
+   _GET_(self%id_lina_X, var%lina_X)
+   _GET_(self%id_lina_A, var%lina_A)
+   _GET_(self%id_lina_E, var%lina_E)
+   _GET_(self%id_lina_L, var%lina_L)
+   _GET_(self%id_lina_D, var%lina_D)
+   _GET_(self%id_lina_N, var%lina_N)
+   _GET_(self%id_lina_P, var%lina_P)
 
 
-_GET_(self%id_Temperature, var%Temperature)  !
+   _GET_(self%id_Temperature, var%Temperature)  !
 !-------------------------------------------------
 
 !Copy Variables into private copy
@@ -334,58 +334,99 @@ _GET_(self%id_Temperature, var%Temperature)  !
 !intermediate results......
 !
 !
-qN=!eqn 10
-qP!eqn 10
-gammaN= !eqn 11
-gammaP= !eqn 12
-R= !eqn 14
-cI !eqn 15
-muX= !eqn 13
-
-!colimitation !eqn 17,18
-cdot !eqn 20
-eta !eqn 16
-n ! eqn 21
-rhox !eqn 22
-wx !eqn 23
+   qN=(self%lina_QXN-var%lina_Q0N)/(var%lina_Q_starN-var%lina_Q0N)              !eqn 10
+   qP=(self%lina_QXP-var%lina_Q0P)/(var%lina_Q_starP-var%lina_Q0P)  !eqn 10
+   gammaN=var%gamma_starN*((var%lina_N*var%lina_AN)/(var%gamma_starN + (var%lina_N*var%lina_AN)))*(1.0_rk-qN) !eqn 11
+   gammaP=var%gamma_starP*((var%lina_P*var%lina_AP)/(var%gamma_starP + (var%lina_P*var%lina_AP)))*(1.0_rk-qP) !eqn 12
+   R=zeta&gammaN !eqn 14
+   cI=1-exp(-alpha*var%PAR/var%mu_max) !eqn 15
+   muX=cI*c*var%mu_max-R !eqn 13
+   n=var%lina_n_star*(1+qN) ! eqn 21
+   c=lina_colimitation(qN,qP,c,cn) !colimitation implemented as function !eqn 17,18
+    cdot= !eqn 20
+   eta=var%lina_E_min+(var%lina_E_max-var%lina_E_max)*(1.0_rk+ 0.5_rk * tanh(var%lina_B_star*cdot-var%lina_B)) !eqn 16
+ 
+   rhox =var%lina_rho_starx*var%lina_dx^(-var%lina_arho) * (1.0_rk-(1.0_rk- var%lina_dx^(-var%lina_arho))*cI*c)!eqn 22
+   wx=1.0_rk/(18.0_rk*var%lina_muw)*(var%lina_rhox-var%lina_rhow)*var%lina_g*var%lina_dx^2.0_rk !eqn 23
 
 
 
 !-------------------------------------------------
 !Calculate RHS of the State equations these rates of change are defined by FABM to be per second
 
-rhsv%lina_X= (self%lina_muX - self%lina_mx -self%lina_wx -self%lina_CX) * var%lina_X !eqn 1
-rhsv%lina_A= lina_C_tot - (self%lina_wa + self%lina_ma + self%lina_kB)m * var%lina_A !eqn 2
-!(E and L ,D  are solved by the external AGG model)
-!rhsv%lina_E= lina_eta * var%lina_X - lina_h * var%lina_A !eqn 3
-!rhsv%lina_L= lina_rL - (self%lina_wl +self%lina_CL)*var%lina_L + self%lina_kB*lina_psi * var%lina_A !eqn 4 
-!rhsv%lina_D= self%lina_mx * var%lina_X - (self%lina_mD + self%lina_wD +self%lina_CD) * var%lina_D + self%lina_kB (1- lina_psi) * var%lina_A !eqn 5
-rhsv%lina_N= - self%lina_gammaN * var%lina_X + self%lina_mD * var%lina_D * var%lina_QDN + self%lina_ma * var%lina_A * var%lina_QAN +self%lina_eN !eqn 6
-rhsv%lina_P= - self%lina_gammaP * var%lina_X + self%lina_mD * var%lina_D * var%lina_QDP + self%lina_ma * var%lina_A * var%lina_QAP +self%lina_eP !eqn 7
+   rhsv%lina_X= (self%lina_muX - self%lina_mx -self%lina_wx -self%lina_CX) * var%lina_X !eqn 1
+   rhsv%lina_A= lina_C_tot - (self%lina_wa + self%lina_ma + self%lina_kB)m * var%lina_A !eqn 2
+   !(E and L ,D  are solved by the external AGG model)
+   !rhsv%lina_E= lina_eta * var%lina_X - lina_h * var%lina_A !eqn 3
+   !rhsv%lina_L= lina_rL - (self%lina_wl +self%lina_CL)*var%lina_L + self%lina_kB*lina_psi * var%lina_A !eqn 4 
+   !rhsv%lina_D= self%lina_mx * var%lina_X - (self%lina_mD + self%lina_wD +self%lina_CD) * var%lina_D + self%lina_kB (1- lina_psi) * var%lina_A !eqn 5
+   rhsv%lina_N= - self%lina_gammaN * var%lina_X + self%lina_mD * var%lina_D * var%lina_QDN + self%lina_ma * var%lina_A * var%lina_QAN +self%lina_eN !eqn 6
+   rhsv%lina_P= - self%lina_gammaP * var%lina_X + self%lina_mD * var%lina_D * var%lina_QDP + self%lina_ma * var%lina_A * var%lina_QAP +self%lina_eP !eqn 7
 
-rhsv%lina_QXN= lina_gammaN- lina_muX * var%lina_QXN !eqn 8
-rhsv%lina_QXP= lina_gammaP- lina_muX * var%lina_QXP !eqn 9
+   rhsv%lina_QXN= lina_gammaN- lina_muX * var%lina_QXN !eqn 8
+   rhsv%lina_QXP= lina_gammaP- lina_muX * var%lina_QXP !eqn 9
 
-rhsv%lina_QDN= self%lina_mx *var%lina_X * var%lina_QXN - ( self%lina_mD + self%lina_wD + self%lina_CD) * var%lina_D * var%lina_QDN + self%lina_kB (1- lina_psi) * var%lina_A * var%lina_QAN !eqn 39
-rhsv%lina_QDP= self%lina_mx *var%lina_X * var%lina_QXP - ( self%lina_mD + self%lina_wD + self%lina_CD) * var%lina_D * var%lina_QDP + self%lina_kB (1- lina_psi) * var%lina_A * var%lina_QAP !eqn 40
+   rhsv%lina_QDN= self%lina_mx *var%lina_X * var%lina_QXN - ( self%lina_mD + self%lina_wD + self%lina_CD) * var%lina_D * var%lina_QDN + self%lina_kB (1- lina_psi) * var%lina_A * var%lina_QAN !eqn 39
 
+   rhsv%lina_QDP= self%lina_mx *var%lina_X * var%lina_QXP - ( self%lina_mD + self%lina_wD + self%lina_CD) * var%lina_D * var%lina_QDP + self%lina_kB (1- lina_psi) * var%lina_A * var%lina_QAP !eqn 40
+
+   rhsv%lina_psi= var%lina_CL*var%lina_L/var%lina_A-var%lina_kB*var%lina_psi-var%lina_psi/var%lina_A* var%lina_dAdt !eqn 24
+   rhsv%lina_QAN= self%lina_Cx *var%lina_X * var%lina_QXN + self%lina_CD* var%lina_D * var%lina_QDN - (self%lina_wa + self%lina_ma + self%lina_kB) * var%lina_A * var%lina_QAN !eqn 25
+    rhsv%lina_QAP= self%lina_Cx *var%lina_X * var%lina_QXP + self%lina_CD* var%lina_D * var%lina_QDP - (self%lina_wa + self%lina_ma + self%lina_kB) * var%lina_A * var%lina_QAP !eqn 26
 
 !---------------------------------------------------
 
 !-----------------------------------------------output of diagnostic
-if Debugout then
-write(*,*) 'diagnostic variables'
- _SET_DIAGNOSTIC_(self%id_lina_muX, lina_muX)                   !Phytoplankton Growth rate
-
-
-
-endif
+   if Debugout then
+    write(*,*) 'diagnostic variables'
+    _SET_DIAGNOSTIC_(self%id_lina_QXN, lina_QXN)
+    _SET_DIAGNOSTIC_(self%id_lina_QXP, lina_QXP)
+    !_SET_DIAGNOSTIC_(self%id_lina_Q_starN,    
+    !_SET_DIAGNOSTIC_(self%id_lina_Q_starP, 
+    _SET_DIAGNOSTIC_(self%id_lina_muX, lina_muX)                   !Phytoplankton Growth rate
+    _SET_DIAGNOSTIC_(self%id_lina_mX, lina_mX)
+    _SET_DIAGNOSTIC_(self%id_lina_wX, lina_wX)
+    _SET_DIAGNOSTIC_(self%id_lina_CX,lina_CX)
+    _SET_DIAGNOSTIC_(self%id_lina_gammaN,lina_gammaN)
+    _SET_DIAGNOSTIC_(self%id_lina_gammaP,lina_gammaP)
+    _SET_DIAGNOSTIC_(self%id_lina_cI,lina_cI)
+    _SET_DIAGNOSTIC_(self%id_lina_c,lina_c)
+    _SET_DIAGNOSTIC_(self%id_lina_n,lina_n)
+    _SET_DIAGNOSTIC_(self%id_lina_alpha,lina_alpha)
+    _SET_DIAGNOSTIC_(self%id_lina_mu_max,lina_mu_max)
+    _SET_DIAGNOSTIC_(self%id_lina_E,lina_E)
+    _SET_DIAGNOSTIC_(self%id_lina_E_min,lina_E_min)
+    _SET_DIAGNOSTIC_(self%id_lina_E_max,lina_E_max)
+    _SET_DIAGNOSTIC_(self%id_lina_C_dot,lina_C_dot)
+    _SET_DIAGNOSTIC_(self%id_lina_B,lina_B)
+    _SET_DIAGNOSTIC_(self%id_lina_B_star,lina_B_star)
+    _SET_DIAGNOSTIC_(self%id_lina_n_star,lina_n_star)
+    
+    _SET_DIAGNOSTIC_(self%id_lina_QAN,lina_QAN)
+    _SET_DIAGNOSTIC_(self%id_lina_QAP,lina_QAP)
+   endif
 
 !-----------------------------------------calculate the ODE
-if Debugout then write(*,*) 'calculate ODE'
-_ADD_SOURCE_(self%id_lina_X,rhsv%lina_X)
-!_SET_ODE_(self%id_lina_X, rhsv%lina_X)
+   if Debugout then write(*,*) 'calculate ODE'
+   _ADD_SOURCE_(self%id_lina_X,rhsv%lina_X)
+   !_SET_ODE_(self%id_lina_X, rhsv%lina_X)
 !--------------------------------------------finalize FABM loops
-!_FABM_LOOP_END_
-_LOOP_END_
+   !_FABM_LOOP_END_
+   _LOOP_END_
   end subroutine do
+
+
+  function lina_colimitation(qi,qj,n,cn) result(c)
+     real(rk), intent(in)::qi,qj,n,cn
+     real(rk)  ::c
+
+     c=qi*lina_gn(qj/qi,n)*(1+qi*qj*n+cn)
+  end function
+  
+  function lina_gn(r,n) result(g)
+    real(rk),intent(in):: r,n
+    real(rk) ::g
+    
+    g=(r-r^(1+n))/(1-r^(1+n))
+  end function
+
